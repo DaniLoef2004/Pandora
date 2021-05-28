3cd04be3abecf671c26d08063a40d71c9bc39ed6
DaniLoef2004 <70745652+DaniLoef2004@users.noreply.github.com>
2021-05-28 07:11:23 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* Template name: 404
+*/
+?>
+
+<?php get_header();?>
+
+<div id="contentholder">
+
+    <a id="content"></a>
+    <div class="scale clearfix">
+        <section id="inhoud">
+            <div id="aboutuswrapper"></div>
+            <div id="inhoudsfullwrapper"></div>
+            <div class="codesnippetsholder clearfix">
+
+                <h1>Oeps, er ging wat mis</h1>
+                <p>Helaas, deze pagina is niet (meer) beschikbaar.</p>
+                <p><a href="/index.php" title="Verder navigeren" class="meerinfoblok">Home</a></p>
+
+            </div></div></div>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+</body>
+</html>
+
+<?php get_footer();?>
